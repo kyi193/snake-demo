6f465d0c1c430f0e78b718b821e186b7d6724381
Kevin <kevin.yi93@gmail.com>
2020-07-23 18:07:32 -0400

    Display game board and snake on HTML/CSS grid

---
+++ index.js
@@ -0,0 +1,32 @@
+import _ from 'lodash'
+import './style.css';
+
+import SnakeGame from './snake/SnakeGame'
+const game = new SnakeGame
+game.snake.moveSnake()
+game.updateGameBoard()
+console.log(game.gameBoard.getBoard())
+
+var table = document.createElement('table');
+var tableBody = document.createElement('tbody');
+
+game.gameBoard.getBoard().forEach(function (rowData) {
+  var row = document.createElement('tr');
+
+  rowData.forEach(function (cellData) {
+    if (cellData === 'O') {
+      var cell = document.createElement('td');
+      cell.className = "empty"
+      row.appendChild(cell);
+    } else {
+      var cell = document.createElement('td');
+      cell.className = "dot"
+      row.appendChild(cell);
+    }
+  });
+
+  tableBody.appendChild(row);
+});
+
+table.appendChild(tableBody);
+document.body.appendChild(table);

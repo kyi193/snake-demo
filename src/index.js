6cd0a76c87e5835be0284260a90b07f086fd00fd
Kevin <kevin.yi93@gmail.com>
2020-07-23 20:16:31 -0400

    Move board display logic into a function

--- src/index.js
+++ src/index.js
@@ -3,25 +3,29 @@ import './style.css';
 
 import SnakeGame from './snake/SnakeGame'
 const game = new SnakeGame
+game.updateGameBoard()
 
 var table = document.createElement('table');
 var tableBody = document.createElement('tbody');
 
-game.gameBoard.getBoard().forEach(function (rowData) {
-  var row = document.createElement('tr');
-  rowData.forEach(function (cellData) {
-    if (cellData === 'O') {
-      var cell = document.createElement('td');
-      cell.className = "empty"
-      row.appendChild(cell);
-    } else {
-      var cell = document.createElement('td');
-      cell.className = "dot"
-      row.appendChild(cell);
-    }
+function displayBoard() {
+  game.gameBoard.getBoard().forEach(function (rowData) {
+    var row = document.createElement('tr');
+    rowData.forEach(function (cellData) {
+      if (cellData === 'O') {
+        var cell = document.createElement('td');
+        cell.className = "empty"
+        row.appendChild(cell);
+      } else {
+        var cell = document.createElement('td');
+        cell.className = "dot"
+        row.appendChild(cell);
+      }
+    });
+    tableBody.appendChild(row);
   });
-  tableBody.appendChild(row);
-});
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+}
 
-table.appendChild(tableBody);
-document.body.appendChild(table);
+displayBoard()
